2edf6b9fa490d093cb8fb9b77493d9c4cecf67f1
Muhammad Umar <114831714+Umar43Umar@users.noreply.github.com>
2024-04-11 11:33:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ import path from 'path'; // Import the 'path' module
 dotenv.config();
 
 connectDB();
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build"))); // Use the 'path' module here
+app.use(express.static(path.join(__dirname, "./client/build"))); 
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
